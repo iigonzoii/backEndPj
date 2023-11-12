2fdb2bbd1ff9fa821618448c8ae78a834cf84ead
iigonzoii <a.gonzalez418@gmail.com>
2023-11-12 10:10:57 -0600

    error handlers

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { enviroment } = require('./config');
 const routes = require ('./routes')
+const { ValidationError } = require('sequelize');
 const isProduction = enviroment === 'production';
 const app = express();
 
@@ -35,8 +36,40 @@ app.use(
     })
 )
 app.use(routes)
+//* catch unhandled requests and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('The requested resource couldn\'t be found.');
+    err.title = 'Resource Not Found';
+    err.errors = {message: 'The requested resource couldn\'t be found.'};
+    err.status = 404
+    next(err)
+});
 
+//* Process sequelize errors
 
+app.use((err, req, res, next) => {
+    if (err instanceof ValidationError) {
+        let errors = {};
+        for (let error of err.errors) {
+            errors[error.path] = error.message;
+        }
+        err.title = 'Validation error';
+        err.errors = errors;
+    }
+    next(err)
+})
+
+//* error formatter
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 
 module.exports = app;
