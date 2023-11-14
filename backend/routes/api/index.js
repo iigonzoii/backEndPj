b5cc9961b04ca301661de05cd33ded801295c374
iigonzoii <a.gonzalez418@gmail.com>
2023-11-14 09:16:24 -0600

    finished auth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,29 +1,26 @@
 const router = require ('express').Router()
-
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-//   });
 const { restoreUser } = require('../../utils/auth.js');
-const { setTokenCookie } = require('../../utils/auth.js');
-
 router.use(restoreUser);
 
-router.get(
-  '/restore-user',
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
 
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: 'Demo-lition'
-    }
-  });
-  setTokenCookie(res, user);
-  return res.json({ user: user });
-});
+// const { User } = require('../../db/models');
+// const { setTokenCookie } = require('../../utils/auth.js');
+
+// router.get(
+//   '/restore-user',
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'Demo-lition'
+//     }
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user: user });
+// });
 
 module.exports = router
