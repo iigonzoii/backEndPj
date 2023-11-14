2e5e686a3798afa40ccc7c2b95aa55dc9cc45661
iigonzoii <a.gonzalez418@gmail.com>
2023-11-13 20:12:03 -0600

    user auth middleware 90% done, bedtime

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,29 @@
 const router = require ('express').Router()
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body });
+//   });
+const { restoreUser } = require('../../utils/auth.js');
+const { setTokenCookie } = require('../../utils/auth.js');
+
+router.use(restoreUser);
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
   });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
 
 module.exports = router
