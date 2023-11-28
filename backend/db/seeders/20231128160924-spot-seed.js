ec0662f412074058da55132c1f5d97e25b8a909e
iigonzoii <a.gonzalez418@gmail.com>
2023-11-28 10:38:52 -0600

    spotseed

---
+++ backend/db/seeders/20231128160924-spot-seed.js
@@ -0,0 +1,63 @@
+'use strict';
+const Spot = require ('../models')
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await Spot.bulkCreate([
+      {
+        ownerId: 1,
+        address: "123 fake street",
+        city: 'fake city',
+        state: 'fake state',
+        country: 'fake country',
+        lat: 37.7645358,
+        lng: -122.4730327,
+        name: "fake name",
+        descripton: 'fake description',
+        price: 5,
+        avgRating: 4.5,
+        previewImage: "image url"
+      },
+      {
+        ownerId: 2,
+        address: "1234 fake street",
+        city: 'fake city',
+        state: 'fake state',
+        country: 'fake country',
+        lat: 37.7645359,
+        lng: -122.4730328,
+        name: "fake name",
+        descripton: 'fake description',
+        price: 6,
+        avgRating: 4.5,
+        previewImage: "image url"
+      },
+      {
+        ownerId: 3,
+        address: "12345 fake street",
+        city: 'fake city',
+        state: 'fake state',
+        country: 'fake country',
+        lat: 37.7645360,
+        lng: -122.4730329,
+        name: "fake name",
+        descripton: 'fake description',
+        price: 7,
+        avgRating: 4.5,
+        previewImage: "image url"
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Spots';
+    return queryInterface.bulkDelete(
+      options, {
+        ownerId: { [Op.in]: [1,2,3] }
+      }, {});
+  }
+};
