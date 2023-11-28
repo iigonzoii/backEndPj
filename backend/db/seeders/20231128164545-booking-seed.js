5691b44e6a9d974eb849769d86fde7789ee70071
iigonzoii <a.gonzalez418@gmail.com>
2023-11-28 10:55:48 -0600

    booking seeder

---
+++ backend/db/seeders/20231128164545-booking-seed.js
@@ -0,0 +1,39 @@
+'use strict';
+const Booking = require('../models')
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await Booking.bulkCreate([
+      {
+        spotId: 1,
+        userId: 1,
+        startDate: '01/01/2024',
+        endDate: '01/02/2024'
+      },
+      {
+        spotId: 2,
+        userId: 2,
+        startDate: '01/02/2024',
+        endDate: '01/03/2024'
+      },
+      {
+        spotId: 3,
+        userId: 3,
+        startDate: '01/03/2024',
+        endDate: '01/04/2024'
+      }
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2, 3] }
+    }, {})
+  }
+};
