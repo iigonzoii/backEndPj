aef2702c4faf762f60190c10afd8da7450eff42d
iigonzoii <a.gonzalez418@gmail.com>
2023-11-28 11:14:54 -0600

    image seeder

---
+++ backend/db/seeders/20231128170521-image-seeder.js
@@ -0,0 +1,39 @@
+'use strict';
+const { Image } = require('../models');
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await Image.bulkCreate([
+      {
+        imageableId: 1,
+        imageableType: "Spot",
+        url: "https://firebasestorage.googleapis.com/v0/b/airbandb-backend-mod4-pj.appspot.com/o/mostCurrentSchema.png?alt=media&token=e449391a-2c4a-48dc-82e9-bfa5de03f047",
+        preview: true
+      },
+      {
+        imageableId: 2,
+        imageableType: "Review",
+        url: "https://firebasestorage.googleapis.com/v0/b/airbandb-backend-mod4-pj.appspot.com/o/mostCurrentSchema.png?alt=media&token=e449391a-2c4a-48dc-82e9-bfa5de03f047",
+        preview: true
+      },
+      {
+        imageableId: 3,
+        imageableType: "Review",
+        url: "url",
+        preview: false
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.table = 'Reviews';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete( options, {
+      imageableId: { [Op.in]: [1, 2, 3]}
+    }, {});
+  }
+};
