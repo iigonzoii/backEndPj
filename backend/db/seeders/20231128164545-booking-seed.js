6af32a671557d95df18d7704ed29c5ef11c11474
iigonzoii <a.gonzalez418@gmail.com>
2023-11-28 18:13:28 -0600

    fixed import by destructuring, forgot to put variable in {}

--- backend/db/seeders/20231128164545-booking-seed.js
+++ backend/db/seeders/20231128164545-booking-seed.js
@@ -1,5 +1,5 @@
 'use strict';
-const Booking = require('../models')
+const {Booking} = require('../models')
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
