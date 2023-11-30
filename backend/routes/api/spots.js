2ab06881a6c0e0d137018e3d80a999d2d6613e9c
iigonzoii <a.gonzalez418@gmail.com>
2023-11-30 11:46:49 -0600

    switching to models branch to add avg rating to spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -10,7 +10,10 @@ const router = express.Router();
 // !start your spot route for get all right away, do nothing else, just start a router.get for all spots
 
 router.get('/', async (req, res, next) => {
-
+    const spots = await Spot.findAll({
+        attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'description', 'createdAt', 'updatedAt', 'avgRating', 'previewImage']
+    })
+    return res.json(spots)
 })
 
 
