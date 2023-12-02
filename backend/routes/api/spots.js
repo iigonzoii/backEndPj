759bde43fbbf9989dbb50757033a6dda83a7e944
iigonzoii <a.gonzalez418@gmail.com>
2023-12-02 13:06:07 -0600

    create a spot done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const { Op } = require('sequelize');
 const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
+const { handleValidationErrors, } = require('../../utils/validation');
+const {requireAuth} = require('../../utils/auth')
 
 const { Spot, Image, User, Review } = require('../../db/models');
 const router = express.Router();
+
+const validateSpot = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Street address is required.'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('City is required.'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage('State is required.'),
+    check('country')
+        .exists({ checkFalsy: true })
+        .withMessage('Country is required.'),
+    check('lat')
+        .exists({ checkFalsy: true })
+        .isFloat({ min: -90, max: 90 })
+        .withMessage('Latitude must be within -90 and 90.'),
+    check('lng')
+        .exists({ checkFalsy: true })
+        .isFloat({ min: -180, max: 180 })
+        .withMessage('Longitude must be within -180 and 180.'),
+    check('name')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 49 })
+        .withMessage('Name must be less than 50 characters'),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage('Description is required'),
+    check('price')
+        .exists({ checkFalsy: true })
+        .isNumeric()
+        .isFloat({min: 0})
+        .withMessage('Price per day must be a positive number'),
+    handleValidationErrors
+];
 
 router.get('/', async (req, res, next) => {
     // empty data obj to house all spots
@@ -44,23 +83,22 @@ router.get('/', async (req, res, next) => {
     return res.json({Spots: data})
 })
 
-// router.post('/', async (req, res, next) => {
-//     const { address, city, state, country, lat, lng, name, description, price } = req.body
-//     // ! getting id as null, and am not getting ownerId at all
-//     const spot = await Spot.create({
-//         include: {model: User, attributes: ['ownerId']},
-//         // id: id,
-//         address:address,
-//         city: city,
-//         state: state,
-//         country: country,
-//         lat: lat,
-//         lng: lng,
-//         name: name,
-//         description: description,
-//         price: price
-//     })
-//     res.status(201).json(spot)
-// })
+router.post('/', requireAuth, validateSpot, async (req, res, next) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body
+    const ownerId = req.user.id
+    const spot = await Spot.create({
+        ownerId,
+        address:address,
+        city: city,
+        state: state,
+        country: country,
+        lat: lat,
+        lng: lng,
+        name: name,
+        description: description,
+        price: price
+    })
+    res.status(201).json(spot)
+})
 
 module.exports = router
