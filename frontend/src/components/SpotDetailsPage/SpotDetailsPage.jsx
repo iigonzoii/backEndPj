da6cabecc1b1d6f6ecf8c3e538a5d0e420979806
iigonzoii <a.gonzalez418@gmail.com>
2024-03-04 15:51:15 -0600

    reserve button on spotDetail

--- frontend/src/components/SpotDetailsPage/SpotDetailsPage.jsx
+++ frontend/src/components/SpotDetailsPage/SpotDetailsPage.jsx
@@ -3,12 +3,14 @@ import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import { fetchSpot } from "../../store/spotReducer"
 import { fetchReviews } from "../../store/reviewReducer"
+import './SpotDetailsPage.css'
 
 
 function SpotDetailsPage() {
     const { spotId } = useParams()
     const dispatch = useDispatch();
     let spot = useSelector(state => state.spot.spotDetail);
+    let reserve = () => alert("Feature coming soon")
 
     let review = useSelector(state => state.review)
     review = Object.values(review)
@@ -19,27 +21,27 @@ function SpotDetailsPage() {
     }, [dispatch, spotId ]);
 
     return (
-        <div>
+        <div className="spot-detail-container">
             <div>
                 {<p>{spot && spot.name}</p>}
                 {<p>{`${spot && spot.city}, ${spot && spot.state}, ${spot && spot.country}`}</p>}
             </div>
-            <div>
+            <div className="img-container">
                 {spot && spot.SpotImages.map((img, index) => (
                         <div key={index}>
                             <img src={img.url} />
                         </div>
                     ))}
             </div>
-            <div>
+            <div className="host-description">
                 {<p>{`Hosted by ${spot && spot.Owner.firstName} ${spot && spot.Owner.lastName}`}</p>}
                 {<p>{spot && spot.description}</p>}
             </div>
-            <aside>
-                <p className="">{`$${spot && spot.price} a night`}</p>
-                <p className="starRating "><i className="fa-solid fa-star"></i>{`${spot && spot.avgStarRating}`}</p>
-                <p className="">{`${spot && spot.numReviews} reviews`}</p>
-                <button className="pointer">Reserve</button>
+            <aside className="reservation-box">
+                <span className="">{`$${spot && spot.price} a night`}</span>
+                <span className="starRating "><i className="fa-solid fa-star"></i>{`${spot && spot.avgStarRating}`}</span>
+                <span >{`${spot && spot.numReviews} reviews`}</span>
+                <div><button className="pointer" onClick={reserve}>Reserve</button></div>
             </aside>
             <div>
             <p className="starRating "><i className="fa-solid fa-star"></i>{`${spot && spot.avgStarRating}`}</p>
