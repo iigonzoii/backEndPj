7a3248cf330a7d0c7ee1919ef3886ddbb668f7b7
iigonzoii <a.gonzalez418@gmail.com>
2024-04-13 16:16:22 -0500

    5+

--- frontend/src/components/UpdateSpotPage/UpdateSpotPage.jsx
+++ frontend/src/components/UpdateSpotPage/UpdateSpotPage.jsx
@@ -22,7 +22,7 @@ function UpdateSpotPage() {
             setPrice(spot.price)
         })
         .then(() => setIsLoaded(true))
-    }, [dispatch])
+    }, [dispatch, spotId])
 
     const [isLoaded, setIsLoaded] = useState(false)
     const [errors, setErrors] = useState({})
